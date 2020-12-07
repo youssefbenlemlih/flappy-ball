334f3875c0cfdaa95a7517922e8f1c140a3ab537
YoussefBenlemlih <YBenlemlih@wernerwirth.de>
2020-12-07 09:16:12 +0100

    code quality

--- sketch.js
+++ sketch.js
@@ -1,23 +1,20 @@
 var score;
 var canvasWidth = 400;
-var canvasHeight = 600; //must be changed in bird.class
+var canvasHeight = 600;
 var score = 0 ;
 var bird;
 var wall,wall2,wall3;
 var walls = {wall,wall2}
 var thewall;
 var gameOver=false;
-var gameOverTimer=0;
- 
+
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   frameRate(40);
-  
   score =0 ;
-  
   bird = new Bird();
   bird.Bird();
-  
+
   wall = new Wall();
   wall2 = new Wall();
   wall3 = new Wall();
@@ -25,39 +22,37 @@ function setup() {
   wall.Wall();
   wall2.Wall();
   wall3.Wall();
-  
-   wall2.resetPosition(1.5);
+
+  wall2.resetPosition(1.5);
   wall3.resetPosition(2);
 }
 
 function draw() {
-  
-  
   background(0);
   fill(99,29,88);
   bird.show();
   bird.fall();
-  
+
   wall.show();
   wall.move();
-  
+
   wall2.show();
   wall2.move();
-  
+
   wall3.show();
   wall3.move();
-  
+
   updateScore();
   if (wall.isOut){
     wall.resetPosition(1.5);
     score += 1 ;
   }
-   
+
   if (wall2.isOut){
     wall2.resetPosition(1.5);
     score += 1 ;
   }
-  
+
   if (wall3.isOut){
     wall3.resetPosition(1.5);
     score += 1 ;
@@ -65,47 +60,44 @@ function draw() {
   bird.checkifdead(wall);
   bird.checkifdead(wall2);
   bird.checkifdead(wall3);
-  
-  
+
   if (bird.touchesWall || bird.touchesSide){
-  gameOver = true
+    gameOver = true
   }
-if (gameOver == true){
-	gameOverTimer+=1;
+  if (gameOver ){
     bird.pause();
     wall.pause();
-     wall2.pause();
-     wall3.pause();
+    wall2.pause();
+    wall3.pause();
     background(255,255,255,127)
-  textSize(32);
-  fill(50,0,0,127);
-  text('  U Lil Looser \n \n Your Score : '+
-       score +"\n Tap To replay!",
-       canvasWidth/2-100, canvasHeight/2);
-    
+    textSize(32);
+    fill(50,0,0,127);
+    text('  Game over \n \n Your Score : '+
+      score +"\n Tap To replay!",
+      canvasWidth/2-100, canvasHeight/2);
   }
 }
 
 function keyPressed() {
-   if(keyCode ===UP_ARROW){
+  if(keyCode ===UP_ARROW){
     if (!bird.isPaused) {
       bird.jump();
     }
-     else {}
-    }
-mouseClicked();
-  
+    else {}
+  }
+  mouseClicked();
 }
+
 function mouseClicked(){
-if(gameOver == true && gameOverTimer >=20){
-setup();gameOver = false;
-gameOverTimer = 0;
+  if(gameOver ){
+    setup();gameOver = false;
+  }
 }
-}
+
 function updateScore(){
   textSize(20);
   fill(255,255,255);
   text(score,canvasWidth-50, 50);
-  }
+}
 
 
